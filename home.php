e66d2826e0b4265891e8c8ea406e4c4317a431eb
Javier Luque <104467449+bjavierluque@users.noreply.github.com>
2022-04-28 16:25:01 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+	$pgtitle="Home";
+	$onload="";
+?>
+
+<?php include ("include/cabecera.php");?>
+
+	<!-- Content -->
+	<section>
+		<header class="main">
+			<h1>Sistema de Producción de Seguros</h1>
+			<span class="image main"><img src="images/pic11.jpg" alt="" /></span>
+		</header>
+	</section>
+
+<?php include ("include/pie.php");?>
